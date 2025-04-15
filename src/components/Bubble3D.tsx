42e140c6daf64ba5bce7a54a0fa6ad9995efed2e
Bigajax <rafaelr.barbon@gmail.com>
2025-04-15 19:11:37 -0300

    Update Bubble3D.tsx

--- src/components/Bubble3D.tsx
+++ src/components/Bubble3D.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
 import './bubble3d.css';
+
+const Bubble3D = () => {
+  return (
+    <div className="bubble-container">
+      <div className="bubble-glass"></div>
+    </div>
+  );
+};
+
+export default Bubble3D;
