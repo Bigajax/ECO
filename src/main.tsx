f7ec6f9676eae50e0cd836401828653f6f4eae76
Bigajax <rafaelr.barbon@gmail.com>
2025-04-16 13:50:51 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Login from "./app/Login";
+// src/main.tsx
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import Login from './Login';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
-);
+function Main() {
+  const [logado, setLogado] = useState(false);
+
+  return (
+    <React.StrictMode>
+      {logado ? <App /> : <Login onLoginSuccess={() => setLogado(true)} />}
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<Main />);
