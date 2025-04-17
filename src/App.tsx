c61999713fc1ee908b081566a54e1f2f01e128e0
Bigajax <rafaelr.barbon@gmail.com>
2025-04-17 11:16:12 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './app/login';
 import Home from './components/Home';
+import EcoBubbleInterface from './components/EcoBubbleInterface'; // Importe o novo componente
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/eco-bubble" element={<EcoBubbleInterface />} /> {/* Adiciona a nova rota */}
         {/* Você pode adicionar mais rotas aqui no futuro */}
       </Routes>
     </BrowserRouter>
