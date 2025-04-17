81990abafb83c01da700ce28e53dddba241ae300
Bigajax <rafaelr.barbon@gmail.com>
2025-04-17 10:54:56 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './app/login';
 import Home from './components/Home';
-import App from './App'; // Importe o componente App que define as rotas
+import App from './App.tsx'; // Importe o componente App que define as rotas
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
