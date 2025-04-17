097f84ddf3d15bd1f9f95c04583395c3668f96a5
Bigajax <rafaelr.barbon@gmail.com>
2025-04-17 10:19:52 -0300

    Update Dashboard.tsx

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,1 +1,11 @@
+// src/Dashboard.tsx
+import React from 'react';
+
+export default function Dashboard() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-green-100">
+      <h1 className="text-4xl font-bold">Bem-vindo ao Dashboard!</h1>
+    </div>
+  );
+}
 
