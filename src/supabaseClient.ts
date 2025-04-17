b002f7ecc4706c2ec9eb7f24da25e26598bd2c9b
Bigajax <rafaelr.barbon@gmail.com>
2025-04-17 13:08:53 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,1 +1,7 @@
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://fmqbzsefjboizldfxwfm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtcWJ6c2VmamJvaXpsZGZ4d2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDMyMzQsImV4cCI6MjA2MDQ3OTIzNH0.5jHpcQrG9gCWx97nVbXQr_wSMsrG1Urumso7YlQStP4';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
