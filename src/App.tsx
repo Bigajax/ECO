ea035c712c0ad8a1cda856fbc39e6a2b9002a8ca
Bigajax <rafaelr.barbon@gmail.com>
2025-04-22 12:28:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './app/login';
+import Login from './app/Login'; // Corrigido aqui
 import Home from './components/Home';
 import EcoBubbleInterface from './components/EcoBubbleInterface';
-import SignUp from './components/SignUp'; // Importe o componente SignUp
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} /> {/* Adicione esta linha para acessar explicitamente /login */}
-        <Route path="/signup" element={<SignUp />} /> {/* Adicione a rota para SignUp */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="/eco-bubble" element={<EcoBubbleInterface />} />
-        {/* Você pode adicionar mais rotas aqui no futuro */}
       </Routes>
     </BrowserRouter>
   );
