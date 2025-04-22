f340a9266158b9fff0f33ad3b3a7f14dd0eb2f55
Bigajax <rafaelr.barbon@gmail.com>
2025-04-22 13:39:36 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './app/login'; // IMPORTAÇÃO CORRIGIDA
 import Home from './components/Home';
 import EcoBubbleInterface from './components/EcoBubbleInterface';
 import SignUp from './components/SignUp';
