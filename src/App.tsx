d464d2061f3ed299e3ec9d3efd7106e62d7721fd
Bigajax <rafaelr.barbon@gmail.com>
2025-04-22 13:24:14 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,17 @@ import EcoBubbleInterface from './components/EcoBubbleInterface';
 import SignUp from './components/SignUp';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/eco-bubble" element={<EcoBubbleInterface />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/eco-bubble" element={<EcoBubbleInterface />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
