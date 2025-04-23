509a01695f72decf98aa29f58bc8c38ee9c9853a
Bigajax <rafaelr.barbon@gmail.com>
2025-04-23 18:10:03 -0300

    Update HomePage.tsx

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,14 +1,32 @@
 // src/app/HomePage.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { supabase } from '../components/supabaseClient';
 
 const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const verifySession = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      if (!session?.user) {
+        navigate('/login');
+      } else {
+        setLoading(false);
+      }
+    };
+
+    verifySession();
+  }, [navigate]);
+
+  if (loading) return <div>Carregando...</div>;
+
   return (
     <div>
       <h1>Bem-vindo à Página Inicial!</h1>
-      {/* Adicione aqui o conteúdo da sua página inicial */}
     </div>
   );
 };
 
 export default HomePage;
-
