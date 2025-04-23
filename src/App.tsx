c75d15a93aad7e448aa45333b41d551eda6656d5
Bigajax <rafaelr.barbon@gmail.com>
2025-04-23 17:00:30 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './app/LoginPage';
 import HomePage from './app/HomePage'; // IMPORTANTE: HomePage com "H" maiúsculo
 import SignupPage from './app/SignupPage';
-import { AuthContextProvider } from './AuthContext'; // Importe o AuthContextProvider
 
 function App() {
   return (
-    <AuthContextProvider> {/* Envolva sua aplicação com o AuthContextProvider */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
