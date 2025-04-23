db8fa442eb04089129765769a0d5bee06ff5d718
Bigajax <rafaelr.barbon@gmail.com>
2025-04-23 16:49:51 -0300

    Update Home.tsx

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,1 +1,13 @@
+// src/app/HomePage.tsx
+import React from 'react';
 
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Página Inicial!</h1>
+      {/* Adicione aqui o conteúdo da sua página inicial */}
+    </div>
+  );
+};
+
+export default HomePage;
