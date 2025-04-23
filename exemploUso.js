2d01435c1198cf27e4a369e74de3c986543af1f5
Bigajax <rafaelr.barbon@gmail.com>
2025-04-23 10:47:22 -0300

    Update exemploUso.js

--- exemploUso.js
+++ exemploUso.js
@@ -1,14 +1,1 @@
-// exemploUso.js
-import { supabase } from './src/supabaseClient.js';
-
-async function testarSupabase() {
-  const { data, error } = await supabase.from('mensagens').select('*');
-
-  if (error) {
-    console.error('Erro ao buscar dados:', error.message);
-  } else {
-    console.log('Dados retornados:', data);
-  }
-}
-
-testarSupabase();
+console.log("Funcionou, o Node achou o arquivo!");
