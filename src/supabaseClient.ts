d6f97c6388c167c14f1fa43d35f3899e4bfe6bab
Bigajax <rafaelr.barbon@gmail.com>
2025-04-23 13:48:21 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fmqbzsefjboizldfxwfm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtcWJ6c2VmamJvaXpsZGZ4d2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDMyMzQsImV4cCI6MjA2MDQ3OTIzNH0.5jHpcQrG9gCWx97nVbXQr_wSMsrG1Urumso7YlQStP4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
