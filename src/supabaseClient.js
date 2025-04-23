1635374d936bff4ac051ea9a20171bc37b14ae64
Bigajax <rafaelr.barbon@gmail.com>
2025-04-23 10:05:06 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://fmqbzsefjboizldfxwfm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // mantém sua chave
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
