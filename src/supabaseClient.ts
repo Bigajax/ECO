d636eaad043b7b219c48698959b7c425da68c8d0
Bigajax <rafaelr.barbon@gmail.com>
2025-04-24 13:37:39 -0300

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,9 +1,11 @@
+// src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log('Supabase URL:', supabaseUrl);
-console.log('Supabase Anon Key:', supabaseAnonKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL ou Anon Key não estão definidos nas variáveis de ambiente.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
