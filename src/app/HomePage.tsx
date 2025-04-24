4c0bff803e0d431f7b26ad67aae4a9f4eb8d33fc
Bigajax <rafaelr.barbon@gmail.com>
2025-04-24 14:21:46 -0300

    Update HomePage.tsx

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
+import { supabase } from 'supabaseClient';
 import { Moon, Compass, Music, Eye } from 'lucide-react';
 
 // Componente principal da HomePage
