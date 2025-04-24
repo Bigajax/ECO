fc9fcdff509e7a439b74ed29f16761768473c478
Bigajax <rafaelr.barbon@gmail.com>
2025-04-24 16:32:49 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+console.log('Iniciando a aplicação a partir de main.tsx...');
+
+try {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} catch (error) {
+  console.error('Erro na renderização em main.tsx:', error);
+}
 
