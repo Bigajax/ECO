c87e84fca85fe80b4481298fb7fc27d96eee9d8e
Bigajax <rafaelr.barbon@gmail.com>
2025-04-24 11:37:05 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './app/LoginPage';
 import HomePage from './app/HomePage'; // IMPORTANTE: HomePage com "H" maiúsculo
-import SignupPage from './app/SignupPage';
+// import SignupPage from './app/SignupPage'; // Remova a importação do SignupPage
 import { AuthContextProvider } from './AuthContext'; // Importe o AuthContextProvider
 
 function App() {
@@ -11,10 +11,13 @@ function App() {
     <AuthContextProvider> {/* Envolva SUA APLICAÇÃO INTEIRA com o AuthContextProvider */}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<LoginPage />} /> {/* Rota principal agora é LoginPage */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          {/* Remova a rota para SignupPage */}
+          {/* <Route path="/signup" element={<SignupPage />} /> */}
+          {/* Redireciona qualquer outra rota desconhecida para a página de login */}
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </BrowserRouter>
     </AuthContextProvider>
