4ab7592b9875a5fb706f43b5ba76a025f4c0a5f7
Bigajax <rafaelr.barbon@gmail.com>
2025-04-25 12:32:51 -0300

    Update EcoBubbleInterface.tsx

--- src/components/EcoBubbleInterface.tsx
+++ src/components/EcoBubbleInterface.tsx
@@ -335,4 +335,13 @@ function EcoBubbleInterface() {
 
            {audioPlayer && (
                 <div className="absolute bottom-28 left-4 bg-white/80 backdrop-blur-lg rounded-md shadow-md p-2">
-                    <button onClick={togglePlayPause} className="focus:
+                    <button onClick={togglePlayPause} className="focus:outline-none">
+                        {isPlaying ? <Lucide.Pause size={20} /> : <Lucide.Play size={20} />}
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default EcoBubbleInterface;
